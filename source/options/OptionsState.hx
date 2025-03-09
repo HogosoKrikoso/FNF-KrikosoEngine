1cc39090a834ba57cc74a1dbc5acdee36bf7f1fe
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-03-09 12:07:32 -0400

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -40,7 +40,6 @@ class OptionsState extends MusicBeatState
 	}
 
 	var selectorLeft:Alphabet;
-	var selectorRight:Alphabet;
 
 	override function create() {
 		#if DISCORD_ALLOWED
@@ -70,16 +69,14 @@ class OptionsState extends MusicBeatState
 
 		for (i in 0...options.length)
 		{
-			var optionText:Alphabet = new Alphabet(0, 0, options[i], true);
-			optionText.screenCenter();
+			var optionText:Alphabet = new Alphabet(65, 0, options[i], true);
+			optionText.screenCenter(Y);
 			optionText.y += (100 * (i - (options.length / 2))) + 50;
 			grpOptions.add(optionText);
 		}
 
 		selectorLeft = new Alphabet(0, 0, '>', true);
 		add(selectorLeft);
-		selectorRight = new Alphabet(0, 0, '<', true);
-		add(selectorRight);
 
 		changeSelection();
 		ClientPrefs.saveSettings();
@@ -153,8 +150,6 @@ class OptionsState extends MusicBeatState
 				item.alpha = 1;
 				selectorLeft.x = item.x - 63;
 				selectorLeft.y = item.y;
-				selectorRight.x = item.x + item.width + 15;
-				selectorRight.y = item.y;
 			}
 		}
 		FlxG.sound.play(Paths.sound('scrollMenu'));
