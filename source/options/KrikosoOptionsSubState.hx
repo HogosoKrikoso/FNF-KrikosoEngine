b6a98886315c25844314b3fc2d6ba46392435237
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-03-09 20:34:06 -0400

    Add files via upload

---
+++ source/options/KrikosoOptionsSubState.hx
@@ -0,0 +1,35 @@
+package options;
+
+import objects.Note;
+import objects.StrumNote;
+import objects.Alphabet;
+
+class KrikosoOptionsSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Krikoso Engine';
+		rpcTitle = 'Krikoso Engine Menu'; //for Discord Rich Presence
+		
+		var option:Option = new Option('Score Text Separator:',
+			"What separator should use the Score Text?",
+			'separator',
+			'string',
+			['Double Slash', 'Minus Symbol', 'Pipe', 'Circle', 'Virgulilla']);
+		addOption(option);
+
+		var option:Option = new Option('Combo on Stage',
+			"If checked, Ratings and Combo will show on Stage instead of UI.",
+			'scoreOnWorld',
+			'bool');
+		addOption(option);
+		
+		var option:Option = new Option('Force Skip Countdown',
+			"If checked, It will force the game to skip the countdown.",
+			'forceSkipCountdown',
+			'bool');
+		addOption(option);
+
+		super();
+	}
+}
