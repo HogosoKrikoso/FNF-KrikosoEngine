1f289f6d1f6a0ec9c820ae4bf9e1f5a7a5e908ec
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-03-23 18:48:46 -0400

    Update Tank.hx

--- source/states/stages/Tank.hx
+++ source/states/stages/Tank.hx
@@ -307,7 +307,7 @@ class Tank extends BaseStage
 				pico.anim.onComplete = function()
 				{
 					pico.anim.play('picoEnd', true);
-					pico.anim.onComplete = function():flixel.util.FlxSignal
+					pico.anim.onComplete = function()
 					{
 						gfGroup.alpha = 1;
 						pico.visible = false;
