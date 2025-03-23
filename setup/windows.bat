d31edbe581e73986f0803b7db9278156c1061e5e
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-03-23 18:57:00 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -3,21 +3,7 @@ color 0a
 cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib git hxcpp https://github.com/MobilePorting/hxcpp --quiet
-haxelib git lime https://github.com/MobilePorting/lime --quiet
-haxelib git openfl https://github.com/MobilePorting/openfl 9.3.3 --quiet
-haxelib git flixel https://github.com/MobilePorting/flixel 5.6.1 --quiet
-haxelib install flixel-addons 3.2.2 --quiet
-haxelib install flixel-tools 1.5.1 --quiet
-haxelib install flixel-ui --quiet
-haxelib install tjson 1.4.0 --quiet
-haxelib git SScript https://github.com/MobilePorting/SScript-7.7.0
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
-haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit --quiet
-haxelib git hxdiscord_rpc https://github.com/MobilePorting/hxdiscord_rpc --quiet --skip-dependencies
-haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
-haxelib git hxCodec https://github.com/MobilePorting/hxCodec --quiet
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
+haxelib install hmm --quiet
+haxelib run hmm install --quiet
 echo Finished!
 pause
