a0bd6928f0769a09a5546ddfdc5400de638313e4
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-03-24 12:51:28 -0400

    Delete setup directory

--- setup/windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-color 0a
-cd ..
-echo Installing dependencies...
-echo This might take a few moments depending on your internet speed.
-haxelib install hmm --quiet
-haxelib run hmm install --quiet
-echo Finished!
-pause
