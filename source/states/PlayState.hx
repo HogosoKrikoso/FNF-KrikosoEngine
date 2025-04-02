81b16fc1bfee58b387552e66d11c97398883fc55
Hog <156556823+HogMedioScorched@users.noreply.github.com>
2025-04-01 21:28:58 -0400

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -733,8 +733,12 @@ class PlayState extends MusicBeatState
 	#end
 
 	public function reloadHealthBarColors() {
-		healthBar.setColors(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
-			FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
+		if(!ClientPrefs.data.rgBar) {
+	        	healthBar.setColors(FlxColor.fromRGB(dad.healthColorArray[0], dad.healthColorArray[1], dad.healthColorArray[2]),
+		        	FlxColor.fromRGB(boyfriend.healthColorArray[0], boyfriend.healthColorArray[1], boyfriend.healthColorArray[2]));
+		} else {
+			healthBar.setColors(0xFF0000, 0x00FF00);
+		}
 	}
 
 	public function addCharacterToList(newCharacter:String, type:Int) {
