9eefc6444eca166a3b8525ffec3668ecbd3819ee
BlipInecesario <130312651+Blip25@users.noreply.github.com>
2025-05-02 22:45:20 -0400

    Update StorageUtil.hx

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -55,7 +55,7 @@ class StorageUtil
 	#if android
 	// always force path due to haxe
 	public static function getExternalStorageDirectory():String
-		return '/sdcard/.PsychEngine073/';
+		return '/sdcard/.KrikosoEngine/';
 
 	public static function requestPermissions():Void
 	{
